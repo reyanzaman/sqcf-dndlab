644e8d6b000cf4aee777f9c03df3ef061a54a286
Khandoker Ashik Uz Zaman <reyanzaman.rz@gmail.com>
2024-03-03 04:36:59 +0600

    potential debugging

--- app/api/getArt/route.ts
+++ app/api/getArt/route.ts
@@ -1,16 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
 const GET = async (req: NextApiRequest, res: NextApiResponse) => {
-    const url = new URL(req.url || '', 'http://localhost');
+    const url = new URL(req.url || '', `http://${req.headers.host}`);
     const id = url.searchParams.get('id');
-    console.log("ID: ", id)
+    console.log("ID: ", id);
 
     if (!id) {
-        return NextResponse.json({ message: "ID Not Found" });
+        return new Response("ID Not Found", {
+            status: 400
+        })
     }
 
     try {
@@ -24,13 +26,25 @@ const GET = async (req: NextApiRequest, res: NextApiResponse) => {
             return NextResponse.json(art);
         } else {
             return new Response("Artwork not found", {
-                status: 500
+                status: 404
             })
         }
     } catch (error) {
         console.error("Failed to get art:", error);
-        return new Response("Error occured during artwork retrieval", {
+        return new Response("Error occurred during artwork retrieval", {
             status: 500
+        })
+    }
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+        return GET(req, res);
+    } else {
+        // Handle any other HTTP methods if necessary
+        res.setHeader('Allow', ['GET']);
+        return new Response(`Method ${req.method} Not Allowed`, {
+            status: 405
         })
     }
 }
