5f90ad4fb53c753005486e5f26351214daa96d45
Khandoker Ashik Uz Zaman <reyanzaman.rz@gmail.com>
2024-03-03 04:47:23 +0600

    added default templates

---
+++ app/tahera/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function About() {
+  return <main className="">
+    <div className="">
+        <h1 className="text-5xl">About Tahera Khanom</h1>
+    </div>
+  </main>;
+}
