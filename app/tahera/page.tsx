9bb1756a3fb4a51c6d29f9f93269f77f1b1160cc
Khandoker Ashik Uz Zaman <reyanzaman.rz@gmail.com>
2024-03-05 00:42:18 +0600

    centered texts

--- app/tahera/page.tsx
+++ app/tahera/page.tsx
@@ -96,7 +96,7 @@ export default function Tahera() {
   return <main className="bg-[#000000] h-[100dvh] w-[100dvw] text-white p-8">
     <div className="h-full w-full flex flex-col items-center justify-center">
         <div>
-          <h1 className="text-5xl">Under Development</h1>
+          <h1 className="text-5xl text-center">Under Development</h1>
         </div>
     </div>
   </main>;
